02836996f700ec11bc80f19714f284812d639cdf
Ömer Turan <omerturan34.14@gmail.com>
2025-09-11 13:07:22 +0300

    feat: add task input & dropdown, refactor browser detection, working on task rendering

--- app.js
+++ app.js
@@ -5,22 +5,76 @@ darkMode.addEventListener('change', function(){
     : (document.body.classList.add('light-theme'), document.body.classList.remove('dark-theme'),console.log("Light mode aktif"));
 });
 
+//Anlık tarayıcı göster
 const detectBrowser = () => {
     const ua = navigator.userAgent; //user agent bilgisini alıyoruz
-    if (ua.includes('Chrome') && !ua.includes('Edg') && !ua.includes('OPR')){
+    switch(true){
+
+    case ua.includes('Chrome') && !ua.includes('Edg') && !ua.includes('OPR'):
         return "Chrome";
-    }else if (ua.includes('Firefox')){
-        return "Firefox";
-    }else if (ua.includes ('Safari') && !ua.includes('Chrome')){
-        return "Safari";
-    } else if (ua.includes('Edg')){
-        return "Edge";
-    }else if (ua.includes('OPR') || ua.includes('Opera')){
-        return "Opera";
-    }else{
-        return "Bilinmeyen tarayıcı";
-    }
-}//if else ile tarayıcıları sıra sıra eliyoruz.
+
+            case ua.includes('Firefox'):
+                return "Firefox";
+
+            case ua.includes('Safari') && !ua.includes('Chrome'):
+                return "Safari";
+
+            case ua.includes('Edg'):
+                return "Edge";
+
+            case ua.includes('OPR') || ua.includes('Opera'):
+                return "Opera";
+
+                default:
+                    return "Bilinmeyen Tarayıcı!";
+    }};
+    //switch case opt.
+//if else ile tarayıcıları sıra sıra eliyoruz.
 const browser = detectBrowser();
 document.getElementById('browser-info').textContent = "Tarayıcınız: " + browser; //fonksiyonu çağırıyoruz ve ekrana basmak için textContent komutunu kullanıyoruz. 
 console.log(navigator.userAgent);
+
+let selectedStatus = null;
+
+//Dropdown'dan statü seçme
+
+document.querySelectorAll('.dropdown-content a').forEach(function(item){
+    item.addEventListener('click', function (e){
+        e.preventDefault();
+        selectedStatus = this.getAttribute('data-status');
+        document.getElementById('dropbtn').textContent = this.textContent
+    });
+});
+
+//Ekle butonu işlevi 
+document.addEventListener('DOMContentLoaded', function(){
+document.getElementById('add-task').addEventListener('click', function () {
+    const taskText = document.getElementById('task-area').value.trim();
+
+    console.log("Seçilen Statü:", selectedStatus);
+console.log("Eklenecek metin:", taskText);
+
+
+    if (!taskText || !selectedStatus){
+        alert('Lütfen görev veya statü seçiniz!');
+        return;
+    }
+    const taskEl = document.createElement('div');
+    taskEl.textContent = taskText;
+    taskEl.classList.add('task-item'); //css için
+    taskEl.style.border = '1px solid #000';
+    taskEl.style.margin = '5px';
+    taskEl.style.padding = '5px';
+    taskEl.style.backgroundColor = 'white';
+    
+    document.getElementById('task-area').value = "";
+
+    //temizle
+    document.getElementById('task-area').value = '';
+    document.getElementById('dropbtn').textContent = 'Status';
+    selectedStatus = null;
+});
+
+});
+
+
